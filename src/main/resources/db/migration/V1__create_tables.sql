ade8f26fb1dc6987497712763010172657563af4
Ivan Varyuhin <varyuhin03@mail.ru>
2024-10-21 22:47:17 +0300

    feat: docker-compose, create_tables.sql

---
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,125 @@
+
+-- USER
+
+create table if not exists user_role (
+    id      serial primary key,
+    name    varchar(40) not null
+);
+
+create table if not exists control_access (
+    id      serial primary key,
+    name    varchar(40) not null
+);
+
+create table if not exists customer (
+    id          serial primary key,
+    name        varchar(40) not null,
+    surname     varchar(40) not null,
+    email       varchar(50) not null,
+    dob         Date not null
+);
+
+create table if not exists account (
+    id          serial primary key,
+    user_id     serial references customer (id) on delete cascade
+);
+
+create table if not exists customer_to_role (
+    id              serial primary key,
+    customer_id     int references customer (id) on delete cascade,
+    role_id         int references user_role (id) on delete cascade
+);
+
+create table if not exists role_to_access (
+    id          serial primary key,
+    role_id     serial references customer (id) on delete cascade,
+    access_id   serial references control_access (id) on delete cascade
+);
+
+-- TICKETS
+
+create type theme as enum ('registration', 'transactions', 'etc');
+create type status as enum ('created', 'in_work', 'done');
+
+create table if not exists ticket (
+    id              serial primary key,
+    theme_id        theme not null,
+    title           varchar(50) not null,
+    description     text not null,
+    status_id       status not null,
+    complainer_id   serial references customer (id) on delete set null,
+    responsible_id  serial references customer (id) on delete set null
+);
+
+create table if not exists ticket_chat_message (
+    id          serial primary key,
+    ticket_id   serial references ticket (id) on delete cascade,
+    create_dttm date not null,
+    message_text    text not null,
+    sender_id   serial references customer (id) on delete cascade
+);
+
+-- BANK
+
+create table if not exists bank_account (
+    id          serial primary key,
+    name        varchar(100) not null,
+    balance     bigint not null
+);
+
+create table if not exists bank (
+    id          serial primary key,
+    name        varchar(50) not null,
+    bank_account_id     serial references bank_account (id) on delete set null
+);
+
+create table if not exists fiat_wallet (
+    id          serial primary key,
+    account_id  serial references account (id) on delete cascade
+);
+
+create table if not exists dollar_exchange_history (
+    id          serial primary key,
+    wallet_id   serial references fiat_wallet (id) on delete cascade,
+    bank_account_id   serial references bank_account (id) on delete cascade
+);
+
+-- STOCK
+
+create table if not exists wallet (
+    id          serial primary key,
+    account_id  serial references account (id) on delete cascade
+);
+
+create table if not exists coin (
+    id          serial primary key,
+    name        varchar(40)
+);
+
+create table if not exists coin_to_wallet (
+    id          serial primary key,
+    coin_id     serial references coin (id) on delete cascade,
+    wallet_id   serial references wallet (id) on delete cascade
+);
+
+create table if not exists stock_account (
+    id          serial primary key,
+    coin_id     serial references coin (id) on delete cascade,
+    amount      bigint not null
+);
+
+create table if not exists coin_price (
+    id              serial primary key,
+    coin_id         serial references coin (id) on delete cascade,
+    price_dollars   int not null
+);
+
+create table if not exists coin_exchange_history (
+    id              serial primary key,
+    coin_id_sell    serial references coin (id) on delete cascade,
+    coin_id_buy    serial references coin (id) on delete cascade,
+    amount          int not null,
+    tax             int not null,
+    customer_id     serial references customer (id) on delete cascade
+);
+
