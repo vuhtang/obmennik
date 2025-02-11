2bc6047afd9dba67d78ec31ca72f358b1596cca7
artemiyjjj <artemiyromanoff@gmail.com>
2025-02-12 00:09:21 +0300

    feat[file-server-stomp]: Finilize stomp endpoint

--- file-server/src/main/java/org/highload/configuration/WebSocketServerConfig.java
+++ file-server/src/main/java/org/highload/configuration/WebSocketServerConfig.java
@@ -1,66 +1,52 @@
 package org.highload.configuration;
 
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.converter.SimpleMessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 import java.util.List;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketServerConfig implements WebSocketMessageBrokerConfigurer {
-//    @Value("${websocket.s3-topic}")
-//    private final String topic;
-//
-//    @Value("${websocket.s3-url}")
-//    private final String urlPrefix;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/storage-ws")
-                .setAllowedOriginPatterns("*");
-//                .withSockJS();
+                .setAllowedOriginPatterns("*")
+                .setHandshakeHandler(new DefaultHandshakeHandler(new TomcatRequestUpgradeStrategy()))
+                .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic", "/queue", "/file");
-//        registry.enableStompBrokerRelay("/queue", "/topic");
-        registry.setApplicationDestinationPrefixes("app");
-        registry.setUserDestinationPrefix("/user");
-        registry.setPathMatcher(new AntPathMatcher("."));
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
         registry.setMessageSizeLimit(4 * 8192);
-        registry.setTimeToFirstMessage(30000);
+        registry.setTimeToFirstMessage(300000);
     }
 
-//    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
-//        messageConverters.add(MappingJackson2MessageConverter());
-//        return true;
-//    }
+    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+        messageConverters.add(new SimpleMessageConverter());
+        return true;
+    }
 
-//    override fun configureWebSocketTransport(registration:WebSocketTransportRegistration) {
-//        registration.setMessageSizeLimit(50 * 1024 * 1024)
-//    }
+    @Bean
+    public ServerEndpointExporter ServerEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
 
-
-//    @Bean
-//    fun createWebSocketContainer(): ServletServerContainerFactoryBean {
-//        val container = ServletServerContainerFactoryBean()
-//        container.setMaxTextMessageBufferSize(1024 * 1024 * 20)
-//        container.setMaxBinaryMessageBufferSize(1024 * 1024 * 20)
-//        container.setMaxSessionIdleTimeout(15 * 60000L)
-//        return container
-//    }
 }
